462f909aa434b94b53c59a63427674ffb32f6048
Jake Bowkett <jake.bowkett01@gmail.com>
2018-12-06 22:34:23 +1100

    Improved comments and added examples.

--- num/num.go
+++ num/num.go
@@ -1,6 +1,5 @@
 /*
-Package num provides some simple functions for converting
-base-10 integers to other bases.
+Package num converts base-10 integers to other bases.
 */
 package num
 
@@ -12,7 +11,19 @@ import (
 
 /*
 Roman converts n to a roman numeral of type string.
-An error will be returned if n is less than one.
+
+Keep in mind there is no roman numeral for zero and that
+the largest value a single roman numeral can represent is
+M (1000). Therefore an n over a few thousand will produce
+results like "MMMMMMMCDXII" (7412) which may be undesired.
+
+	s1, _ := Roman(0)    // This will return an error.
+	s2, _ := Roman(1)    // s2 is "I"
+	s3, _ := Roman(4)    // s3 is "IV"
+	s4, _ := Roman(467)  // s4 is "CDLXVII"
+	s5, _ := Roman(1991) // s5 is "MCMXCI"
+
+Returns an error if n is less than one.
 */
 func Roman(n int) (string, error) {
 	if n == 0 {
@@ -54,7 +65,12 @@ func repeat(s string, n int) string {
 /*
 Alpha converts n to a base 52 string where each numeral
 is represented by an upper or lower case alphabet character.
-An error will be returned if n is negative.
+
+	s1, _ := Alpha(0)  // s1 is "A"
+	s2, _ := Alpha(25) // s2 is "Z"
+	s3, _ := Alpha(52) // s3 is "AA"
+
+Returns an error if n is negative.
 */
 func Alpha(n int) (string, error) {
 	encoding := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -63,11 +79,16 @@ func Alpha(n int) (string, error) {
 
 /*
 Encode converts n to a string that uses the characters
-in encoding as its basis. The base of the result will
-be determined by the number of characters (not bytes)
-in encoding.
+in encoding as its numerals. Multi-byte characters such
+as kanji, emojis, and so on will be treated as a single
+character. The base of the result will be determined by
+the number of characters (not bytes) in encoding.
 
-Adapted from: http://bideowego.com/base-26-conversion
+	s1, _ := Encode(0, "世界") // s1 is "世"
+	s2, _ := Encode(1, "世界") // s2 is "界"
+	s3, _ := Encode(2, "世界") // s3 is "世世"
+
+Returns an error if n is negative.
 */
 func Encode(n int, encoding string) (string, error) {
 
@@ -76,30 +97,19 @@ func Encode(n int, encoding string) (string, error) {
 			fmt.Sprintf("Input number cannot be negative. Got %d.", n))
 	}
 
+	n++ // Our loop below won't begin if n was 0.
 	result := ""
-	n++
-
 	quotient := n
-	var remainder int
+	remainder := 0
 
-	// Get number of characters in encoding rather than
-	// the number of bytes.
+	// Get a slice of the characters (rather than the bytes) in encoding.
 	enc := chars(encoding)
 	length := len(enc)
 
-	// Until quotient of 0.
 	for quotient != 0 {
-
-		// Compensate for zero-based indexing.
-		decremented := quotient - 1
-
-		// Divide by our encoding length.
+		decremented := quotient - 1 // Compensate for zero-based indexing.
 		quotient = decremented / length
-
-		// Get remainder.
 		remainder = decremented % length
-
-		// Prepend letter at index of remainder.
 		result = enc[remainder] + result
 	}
 
